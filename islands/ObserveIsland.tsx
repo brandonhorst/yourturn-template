104fba63900f058c2edebeb174f87d2db084ca95
Brandon Horst <brandonhorst@gmail.com>
2025-12-24 22:48:53 -0500

    Testing

--- islands/ObserveIsland.tsx
+++ islands/ObserveIsland.tsx
@@ -1,14 +1,14 @@
 import { useObserveSocket } from "yourturn/hooks";
-import { ObserverState } from "@game/types.ts";
-import { ObserverView } from "@components/gameviews.tsx";
+import { ObserverState } from "../game/types.ts";
+import { ObserverView } from "../components/gameviews.tsx";
 import { ObserverProps } from "yourturn/types";
 
 export default function ObserveIsland(
   props: { gameId: string; initialObserverProps: ObserverProps<ObserverState> },
 ) {
-  const { observerState, isComplete } = useObserveSocket(
+  const observerProps = useObserveSocket(
     `/observe/${props.gameId}/socket`,
     props.initialObserverProps,
   );
-  return <ObserverView observerState={observerState} isComplete={isComplete} />;
+  return <ObserverView {...observerProps} />;
 }
