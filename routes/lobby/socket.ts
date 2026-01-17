856e133a813625136dc46035aafe4b06abf77d49
Brandon Horst <brandonhorst@gmail.com>
2026-01-17 16:07:36 -0500

    Improve

--- routes/lobby/socket.ts
+++ routes/lobby/socket.ts
@@ -1,14 +1,14 @@
 import { getGameServer } from "../../gameserver.ts";
-import { define, YOURTURN_TOKEN_COOKIE } from "../../utils.ts";
-import { getCookies } from "@std/http/cookie";
+import { checkAuth, define } from "../../utils.ts";
+import { assert } from "@std/assert";
 
 export const handler = define.handlers({
   async GET(ctx) {
     const req = ctx.req;
     const { socket, response } = Deno.upgradeWebSocket(req);
 
-    const cookies = getCookies(ctx.req.headers);
-    const token = cookies[YOURTURN_TOKEN_COOKIE];
+    const token = checkAuth(ctx.req.headers);
+    assert(token != null);
     await getGameServer().configureLobbySocket(socket, token);
 
     return response;
