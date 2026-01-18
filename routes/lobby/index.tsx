71045a86dac81fd824a5f95818dbde36897281ce
Brandon Horst <brandonhorst@gmail.com>
2026-01-17 20:03:55 -0500

    Auth

--- routes/lobby/index.tsx
+++ routes/lobby/index.tsx
@@ -1,24 +1,15 @@
 import LobbyIsland from "../../islands/LobbyIsland.tsx";
 import { getGameServer } from "../../gameserver.ts";
-import { define, YOURTURN_TOKEN_COOKIE } from "../../utils.ts";
+import { checkAuth, define, setAuth } from "../../utils.ts";
 import type { LobbyProps } from "yourturn/types";
-import { getCookies, setCookie } from "@std/http/cookie";
 
 export const handler = define.handlers({
   async GET(ctx) {
     const headers = new Headers();
-    const cookies = getCookies(ctx.req.headers);
-    const providedToken = cookies[YOURTURN_TOKEN_COOKIE];
+    const providedToken = checkAuth(ctx.req.headers);
     const { props, token } = await getGameServer()
       .getInitialLobbyProps(providedToken);
-    setCookie(headers, {
-      name: YOURTURN_TOKEN_COOKIE,
-      value: token,
-      httpOnly: true,
-      sameSite: "Lax",
-      path: "/",
-      maxAge: 60 * 60 * 24 * 30,
-    });
+    setAuth(headers, token);
 
     return { data: props, headers };
   },
